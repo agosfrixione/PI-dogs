888b7c467bbab0fad016dc983f0486d4eead547c
Agos <agosfrixione@gmail.com>
2022-08-18 13:43:52 -0300

    db

--- PI-Dogs-main/api/src/models/Dog.js
+++ PI-Dogs-main/api/src/models/Dog.js
@@ -7,6 +7,32 @@ module.exports = (sequelize) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+    height: {
+      type: DataTypes.INTEGER, 
+      allowNull: false
+    },
+    weight: {
+      type: DataTypes.INTEGER, 
+      allowNull: false
+    },
+    lifeSpan: {
+      type: DataTypes.STRING, 
+      allowNull: true
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    created: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    }
     },
   });
 };
